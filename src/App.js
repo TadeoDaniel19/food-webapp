06600564541e015724406ac78a852f56d74e40d9
Tadeo Daniel <hector_lectric@hotmail.com>
2020-03-04 07:45:01 -0600

    163111/hector.daniel/HOC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from 'react'
+
+const Header = () => {
+  const subtitleStyles = {
+    fontWeight: 'bold'
+  }
+
+  const headerStyles  = {
+    margin: '0.6em',
+    borderRadius: '0.3em',
+    border: '1px solid #d2d2d2',
+    padding: '2em 0.4em',
+    fontFamily: 'monospace',
+    fontSize: '17px',
+    textAlign: 'center'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header style={headerStyles}>
+      <div>
+        ( Hijo â†â†’ Padre )
+      </div>
+      <div style={subtitleStyles}>
+        <span>
+          HOC
+        </span>
+        <br/>
+        <span>
+          High Order Component 
+        </span>
+        <span role='img' aria-label='flame' >
+          ðŸ”¥
+        </span>
+      </div>
+    </header>
+  )
 }
 
-export default App;
+const boxStyles = {
+  padding: '0.5em',
+  margin: ' 0.5em',
+  border: '1px solid gray',
+  borderRadius: '0.3em',
+  textAlign: 'center'
+
+}
+
+const withSizes = (Comp) => class extends Component {
+  state = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize)    
+  }
+
+  handleResize = () => {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+  
+  render () {
+    const { width, height } = this.state
+
+    return (
+      <Comp
+        width={width}
+        height={height}
+      />
+    )
+  }
+}
+
+class App extends Component {
+  render () {
+    const { width, height } = this.props 
+
+    return (
+      <div style={boxStyles}>
+        <Header />
+        <h1>
+          { width } - { height }
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default withSizes(App)
